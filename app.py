6bbb1d98e0cdbf34e3546c2cdb09d91eead3d914
wentao <wm223@ic.ac.uk>
2024-11-09 00:08:48 -0500

    front end

---
+++ app.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import hmac
+
+import os
+from helpers import text_to_speech, autoplay_audio, speech_to_text
+from generate_answer import base_model_chatbot, with_pdf_chatbot
+from audio_recorder_streamlit import audio_recorder
+from streamlit_float import *
+
+def main():
+    float_init()
+
+    def initialize_session_state():
+        if "messages" not in st.session_state:
+            st.session_state.messages = [
+                {"role": "assistant", "content": "Hi there, welcome to the CozyRoom, chat with me about any thing you are worrying about"}
+            ]
+        if "button_flag" not in st.session_state:
+            st.session_state.button_flag = False
+
+
+    initialize_session_state()
+
+    st.title("CoZY Room")             
+
+    
+    footer_container = st.container()
+    with footer_container:
+        col1, col2 = st.columns([4,3])
+        with col1:
+            audio_bytes = audio_recorder(text='Click the Mic and begin speaking', icon_name='microphone', icon_size='2x',)
+        with col2:
+            st.session_state.button_flag = st.toggle("Ask for specific info")
+        selection_container = st.container()
+            
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+
+    if audio_bytes:
+        with st.spinner("Transcribing..."):
+            webm_file_path = "temp_audio.mp3"
+            with open(webm_file_path, "wb") as f:
+                f.write(audio_bytes)
+
+            transcript = speech_to_text(webm_file_path)
+            if transcript:
+                st.session_state.messages.append({"role": "user", "content": transcript})
+                with st.chat_message("user"):
+                    st.write(transcript)
+                os.remove(webm_file_path)
+
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking🤔..."):
+                if not st.session_state.button_flag:
+                    final_response = base_model_chatbot(st.session_state.messages)
+                elif st.session_state.button_flag:
+                    print('--------->', st.session_state.messages)
+                    final_response = with_pdf_chatbot(st.session_state.messages)
+            # with st.spinner("Generating audio response..."):
+                # audio_file = text_to_speech(final_response)
+                # autoplay_audio(audio_file)
+            st.write(final_response)
+            st.session_state.messages.append({"role": "assistant", "content": final_response})
+            # os.remove(audio_file)
+
+    # Float the footer container and provide CSS to target it with
+    footer_container.float("bottom: 0rem;")
+ 
+if __name__ == "__main__":
+
+    main()
---
+++ generate_answer.py
@@ -0,0 +1,99 @@
+import os
+from glob import glob
+
+import openai
+from openai import OpenAI
+from dotenv import load_dotenv
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.chat_models import ChatOpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA
+from langchain.memory import ConversationBufferMemory
+
+
+load_dotenv()
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+client = OpenAI(api_key=openai_api_key)
+openai.api_key = openai_api_key
+
+
+SYSTEM_PROMPT = "You are a student stress relief assistant. Your goal is to use your own words and stories to keep chatting with the user and help them feel better. You have to remember these chatting paradigms and generate your words:\n1. Active Listening: Encourage individuals to express their emotions without judgment, and offer validation by reflecting back on what they are saying to show empathy.\n2. The experience and effects of emotional support: Use empathetic communication, gentle reassurance, and emotional validation when offering support to someone stressed.\n3. Peer support: Make yourself a supportive peer, emphasizing shared experiences to create emotional bonds that help reduce stress."
+
+
+def base_model_chatbot(messages):
+    system_message = [
+        {"role": "system", "content": SYSTEM_PROMPT}]
+    messages = system_message + messages
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        temperature=1,
+        messages=messages
+    )
+    return response.choices[0].message.content
+
+
+class VectorDB:
+    """Class to manage document loading and vector database creation."""
+    
+    def __init__(self, docs_directory:str):
+
+        self.docs_directory = docs_directory
+
+    def create_vector_db(self):
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+
+        files = glob(os.path.join(self.docs_directory, "*.pdf"))
+
+        loadPDFs = [PyPDFLoader(pdf_file) for pdf_file in files]
+
+        pdf_docs = list()
+        for loader in loadPDFs:
+            pdf_docs.extend(loader.load())
+        chunks = text_splitter.split_documents(pdf_docs)
+            
+        return Chroma.from_documents(chunks, OpenAIEmbeddings()) 
+    
+class ConversationalRetrievalChain:
+    """Class to manage the QA chain setup."""
+
+    def __init__(self, model_name="gpt-4o-mini", temperature=1):
+        self.model_name = model_name
+        self.temperature = temperature
+      
+    def create_chain(self):
+
+        model = ChatOpenAI(model_name=self.model_name,
+                           temperature=self.temperature,
+                           )
+
+        memory = ConversationBufferMemory(
+            memory_key="chat_history",
+            return_messages=True
+            )
+        vector_db = VectorDB('docs/')
+        # retriever = vector_db.create_vector_db().as_retriever(search_type="similarity",
+        #                                                       search_kwargs={"k": 2},
+        #                                                       )
+        retriever = vector_db.create_vector_db().as_retriever(search_type="similarity_score_threshold",
+                                                              search_kwargs={'score_threshold': 0.2},
+                                                              )
+
+        return RetrievalQA.from_chain_type(
+            llm=model,
+            retriever=retriever,
+            memory=memory,
+            )
+    
+def with_pdf_chatbot(messages):
+    """Main function to execute the QA system."""
+    query = messages[-1]['content'].strip()
+
+    qa_chain = ConversationalRetrievalChain().create_chain()
+
+    result = qa_chain({"query": query})
+    return result['result']
